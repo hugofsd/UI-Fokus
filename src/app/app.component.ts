2555be683e56e873abc47c57a093ea94fce51cf6
hugofsd <hugo619@outlook.com>
2025-08-30 00:43:23 -0300

    Construindo atumaticas na PWA e motor de atualizações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { UpdateService } from './shared/services/update.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ng-fokus';
+export class AppComponent implements OnInit {
+  constructor(private updateService: UpdateService) {}
+
+  async ngOnInit() {
+    const hasUpdate = await this.updateService.checkForUpdate();
+
+    if (hasUpdate) {
+      console.log('Atualização encontrada durante a inicialização');
+    }
+  }
 }
